9e73602b89419955d1a0d7f348f052d627752232
BOADI DANSO <kwameadansonia123@gmail.com>
2024-05-19 08:52:50 +0000

    Task 2

--- arrayManipulations.js
+++ arrayManipulations.js
@@ -12,3 +12,20 @@ function processArray(arr) {
  const myArray = [11, 12, 13, 14, 15];
  const newArray = processArray(myArray);
  console.log(newArray);
+
+ // Task two adding a formatStringArray function 
+
+function formatArrayStrings(name,newArray) {
+    return name.map((name, index) => {
+        if (newArray[index] % 2 === 0) {
+          return name.toUpperCase();
+        } else {
+          return name.toLowerCase();
+        }
+      });
+
+}
+const name = ['Enoch', 'Emmanuel', 'Bernard', 'Frank', 'Albert'];
+const formattedNames = formatArrayStrings(name, newArray);
+console.log(formattedNames);
+    
